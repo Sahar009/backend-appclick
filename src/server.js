18828b61beab02e14ed95b2b46b51bcaa3e51c33
Akinwumi sehinde <102046091+Sahar009@users.noreply.github.com>
2023-08-17 15:23:43 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -33,7 +33,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000','https://vercel.com/sahar009/frontend'],
     credentials:true
 }))
 
